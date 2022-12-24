e3010f359438f9f3d856dd27c8692c258872daec
Amateur_Hacker <hackerhacked262004@gmail.com>
2022-12-24 21:47:01 +0530

    Adding my neovim configurations

---
+++ .config/nvim/lua/amateur_hacker/plugins/colorizer.lua
@@ -0,0 +1,9 @@
+require("colorizer").setup()
+-- import comment plugin safely
+local setup, colorizer = pcall(require, "colorizer")
+if not setup then
+  return
+end
+
+-- enable comment
+colorizer.setup()
---
+++ .config/nvim/lua/amateur_hacker/plugins/highlights.lua
@@ -0,0 +1,7 @@
+-- Highlight the region on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = num_au,
+  callback = function()
+    vim.highlight.on_yank({ higroup = "Visual", timeout = 120 })
+  end,
+})
